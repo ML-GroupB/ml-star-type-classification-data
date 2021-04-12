ddb519e11480d2a9fbbde1d43b9d744590d8ddc4
kappa243 <edek15pl@gmail.com>
2021-04-12 19:52:09 +0200

    fix comment

--- model_SVC.py
+++ model_SVC.py
@@ -1,8 +1,7 @@
-import matplpotlib.pyplot as plt
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 
 # Function that create and test model and then return the model fit to whole dataset
@@ -17,27 +16,30 @@ def svc_linear(X, y):
     return svc
 
 
+"""
+
 # Testing which kernel is the best
 
-# def training(function_type, c_number):
-#     svm = SVC(kernel=function_type, C=c_number)
-#     svm.fit(X_train, y_train)
-#
-#     predictions = svm.predict(X_test)
-#
-#     return accuracy_score(y_test, predictions)
+def training(function_type, c_number):
+    svm = SVC(kernel=function_type, C=c_number)
+    svm.fit(X_train, y_train)
 
-# c_values = [i/5 for i in range(1, 501, 5)]
-#
-# acc_values = [0.0]*len(c_values)
-#
-# types = {'linear', 'sigmoid', 'rbf', 'poly'}
-# for tpe in types:
-#     for i in range(len(acc_values)):
-#         acc_values[i] = training(tpe, c_values[i])
-#     plt.plot(c_values, acc_values)
-#     plt.title(label=tpe)
-#     plt.show()
+    predictions = svm.predict(X_test)
+
+    return accuracy_score(y_test, predictions)
+
+c_values = [i/5 for i in range(1, 501, 5)]
+
+acc_values = [0.0]*len(c_values)
+
+types = {'linear', 'sigmoid', 'rbf', 'poly'}
+for tpe in types:
+    for i in range(len(acc_values)):
+        acc_values[i] = training(tpe, c_values[i])
+    plt.plot(c_values, acc_values)
+    plt.title(label=tpe)
+    plt.show()
+"""
 
 if __name__ == '__main__':
     stars_data = pd.read_csv('Stars.csv')
